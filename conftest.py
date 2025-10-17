7efbfe83f0079d2a8eec872c3e2dc4508f215e8d
MarkI <menarik@ukr.net>
2025-10-17 23:58:33 +0300

    edited conftest

--- conftest.py
+++ conftest.py
@@ -2,6 +2,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from pages.dashboard_page import DashBoardPage
+from pages.login_page import LoginPage
+from pages.profile_page import PersonalPage
+from utils.Data import Data
+
+
 @pytest.fixture(scope="class", autouse=True)
 def driver(request):
     options = Options()
@@ -20,7 +26,7 @@ def driver(request):
 @pytest.fixture(autouse=True)
 def setup(request, driver):
     request.cls.driver = driver
-    # request.cls.data = Data()
-    # request.cls.login_page = LoginPage(driver)
-    # request.cls.dashboard_page = DashBoardPage(driver)
-    # request.cls.personal_page = PersonalPage(driver)
+    request.cls.data = Data()
+    request.cls.login_page = LoginPage(driver)
+    request.cls.dashboard_page = DashBoardPage(driver)
+    request.cls.personal_page = PersonalPage(driver)
