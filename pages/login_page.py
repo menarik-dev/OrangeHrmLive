f946b16c8eb9a1ea04dc77f9a685072d7838df29
MarkI <menarik@ukr.net>
2025-10-17 22:34:42 +0300

    added login page-object

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from base.base_page import BasePage
+from utils.WaitUntil import WaitUntil
+from utils.Links import Links
+
+class LoginPage(BasePage):
+    PAGE_URL = Links.LOGIN_PAGE
+
+    # Locators
+    USERNAME_FIELD = ("xpath", "//input[@name='username']")
+    PASSWORD_FIELD = ("xpath", "//input[@name='password']")
+    SUBMIT_BUTTON = ("xpath", "//button[@type='submit']")
+    # ERROR_MESSAGE = ("xpath", "//div[@role='alert']")
+    # EXPECTED_ERROR_MESSAGE = "Invalid credentials"
+
+    def Login(self, login, password):
+        WaitUntil.InputText(self.driver, self.USERNAME_FIELD, login, 10  )
+        WaitUntil.InputText(self.driver, self.PASSWORD_FIELD, password, 10  )
+        WaitUntil.ClickElement(self.driver, self.SUBMIT_BUTTON, 10)
