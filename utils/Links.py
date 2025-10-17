ac5b6afb7627f99be34336d0dc1e1e2bdbca3ef9
MarkI <menarik@ukr.net>
2025-10-17 22:04:11 +0300

    added links

---
+++ utils/Links.py
@@ -0,0 +1,6 @@
+class Links:
+    BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php"
+
+    LOGIN_PAGE = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    DASHBOARD_PAGE = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    PERSONAL_PAGE = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7"
