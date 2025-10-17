ca70d0bb4fd634c299a612dbbd56475eebf31241
MarkI <menarik@ukr.net>
2025-10-17 21:14:57 +0300

    edited conftest

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="class", autouse=True)
+def driver(request):
+    options = Options()
+
+    # options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+    request.cls.driver = driver
+    yield driver
+
+    driver.quit()
+
+@pytest.fixture(autouse=True)
+def setup(request, driver):
+    request.cls.driver = driver
+    # request.cls.data = Data()
+    # request.cls.login_page = LoginPage(driver)
+    # request.cls.dashboard_page = DashBoardPage(driver)
+    # request.cls.personal_page = PersonalPage(driver)
