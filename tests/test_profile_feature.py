7d2ee7bb144297834e92a3f75fc471278e967711
MarkI <menarik@ukr.net>
2025-10-18 00:08:13 +0300

    created first test

---
+++ tests/test_profile_feature.py
@@ -0,0 +1,11 @@
+from base.base_test import BaseTest
+
+class TestProfileFeature(BaseTest):
+    def test_change_name(self, driver):
+        self.login_page.open()
+        self.login_page.Login(login=self.data.LOGIN, password=self.data.PASSWORD)
+        self.dashboard_page.is_opened()
+        self.dashboard_page.go_to_profile()
+        self.personal_page.change_name("Stefan")
+        self.personal_page.save_changes()
+        self.personal_page.is_changes_saved()
