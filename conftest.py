716ba231e33f9a271d894a5b736c2d4a35d8fcd3
MarkI <menarik@ukr.net>
2025-10-18 13:20:39 +0300

    allowed a headless mode

--- conftest.py
+++ conftest.py
@@ -12,7 +12,7 @@ from utils.Data import Data
 def driver(request):
     options = Options()
 
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
