3059f10c4604333715e19b68b620e2bc31e8071b
MarkI <menarik@ukr.net>
2025-10-18 12:27:41 +0300

    added allure to base page

--- base/base_page.py
+++ base/base_page.py
@@ -1,11 +1,22 @@
 from utils.WaitUntil import WaitUntil
+from allure_commons.types import AttachmentType
+import allure
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
     def open(self):
-        self.driver.get(self.PAGE_URL)
+        with allure.step(f"Open {self.PAGE_URL} page"):
+            self.driver.get(self.PAGE_URL)
 
     def is_opened(self):
-        WaitUntil.IsPageOpened(self.driver, 10, self.PAGE_URL)
+        with allure.step(f"Page {self.PAGE_URL} is opened"):
+            WaitUntil.IsPageOpened(self.driver, 10, self.PAGE_URL)
+
+    def make_screenshot(self, screenshot_name):
+        allure.attach(
+            body= self.driver.get_screenshot_as_png(),
+            name= screenshot_name,
+            attachment_type= AttachmentType.PNG
+        )
