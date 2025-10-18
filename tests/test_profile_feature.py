fd472c69eb9b0c33a601c3a338c15a00d8b85dcb
MarkI <menarik@ukr.net>
2025-10-18 16:57:23 +0300

    added a variable and edited test

--- tests/test_profile_feature.py
+++ tests/test_profile_feature.py
@@ -12,6 +12,6 @@ class TestProfileFeature(BaseTest):
         self.login_page.Login(login=self.data.LOGIN, password=self.data.PASSWORD)
         self.dashboard_page.is_opened()
         self.dashboard_page.go_to_profile()
-        self.personal_page.change_name("Stefan")
+        self.personal_page.change_name(self.data.NEW_NAME)
         self.personal_page.save_changes()
         self.personal_page.is_changes_saved()
--- utils/Data.py
+++ utils/Data.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
+from utils.TestDataGenerator import TestGenerateData as tgd
 load_dotenv()
 
 class Data:
     LOGIN = os.getenv("LOGIN")
     PASSWORD = os.getenv("PASSWORD")
+    NEW_NAME = tgd.random_string(5, True)
